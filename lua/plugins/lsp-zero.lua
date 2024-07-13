0abc7527d2a70d2e134e20426ad7e62dd8a76831
Jermaine Kelly <jerjunkel@gmail.com>
2024-07-13 01:30:34 -0400

    add typescript lsp

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -6,28 +6,20 @@ return {
       lsp_zero.extend_lspconfig()
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls","tsserver"}
+        ensure_installed = {"lua_ls","tsserver", "eslint"}
       });
       lsp_zero.on_attach(function(client, bufnr)
         lsp_zero.default_keymaps({buffer = bufnr})
         end)
       local lspconfig = require("lspconfig");
       lspconfig.lua_ls.setup({});
+      lspconfig.tsserver.setup({});
     end
   },
 
-  { "williamboman/mason.nvim"},
+  {"williamboman/mason.nvim"},
   {"williamboman/mason-lspconfig.nvim"},
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      --[[
-      local lspconfig = require("lspconfig");
-      //lspconfig.lua_ls.setup({});
-      --]]
-    end;
-  },
+  {"neovim/nvim-lspconfig"},
   {'hrsh7th/cmp-nvim-lsp'},
   {'hrsh7th/nvim-cmp'},
   {'L3MON4D3/LuaSnip'},
