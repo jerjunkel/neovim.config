e40594dff74015267d38b3e8dc5569899ff2923f
Jermaine Kelly <jerjunkel@gmail.com>
2024-07-24 13:53:11 -0400

    change tab spacing to 2 and add autoindent

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
-vim.cmd("set shiftwidth=4") 
-vim.cmd("set tabstop=4") 
-vim.cmd("set softtabstop=4") 
+vim.cmd("set shiftwidth=2") 
+vim.cmd("set tabstop=2") 
+vim.cmd("set softtabstop=2") 
 vim.cmd("set expandtab")
 vim.cmd("set rnu")
 vim.cmd("set nu")
+vim.cmd("set autoindent")
 
 require("config.lazy");
