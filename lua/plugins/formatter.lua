66aed8d961dc21c689efe6050c6c51186d03b3ad
Jermaine Kelly <jerjunkel@gmail.com>
2024-07-26 22:56:12 -0400

    add conform formatter plugin

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,21 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				scss = { "prettierd" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		})
+	end,
+}
