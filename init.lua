a3aec5b09be04eaa0daa9ff8bae21f9c6a7f1f84
Jermaine Kelly <jerjunkel@gmail.com>
2024-07-29 19:01:27 -0400

    add ufo for code folding

--- init.lua
+++ init.lua
@@ -1,16 +1,21 @@
-vim.cmd("set shiftwidth=2") 
-vim.cmd("set tabstop=2") 
-vim.cmd("set softtabstop=2") 
+vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
 vim.cmd("set expandtab")
 vim.cmd("set rnu")
 vim.cmd("set nu")
 vim.cmd("set autoindent")
 
-require("config.lazy");
-require('telescope').setup{ 
-  defaults = { 
-    file_ignore_patterns = { 
-      "node_modules" 
-    }
-  }
-}
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+require("config.lazy")
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			"node_modules",
+		},
+	},
+})
