f8e19ca0a9273341f24e5a6f77f2f21d5b13c8dd
Jermaine Kelly <jerjunkel@gmail.com>
2024-08-05 19:55:32 -0400

    add ufo config file

---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,15 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		local ufo = require("ufo")
+		vim.keymap.set("n", "zR", ufo.openAllFolds)
+		vim.keymap.set("n", "zM", ufo.closeAllFolds)
+
+		ufo.setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "treesitter", "indent" }
+			end,
+		})
+	end,
+}
