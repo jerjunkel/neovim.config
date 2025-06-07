9a6f5bd34886957dc9a4a0edff1c43678eaf3b7b
Jermaine Kelly <jerjunkel@gmail.com>
2025-06-07 18:25:30 -0400

    chore: rename vue language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "cssls", "emmet_ls", "volar", "gopls" },
+				ensure_installed = { "lua_ls", "ts_ls", "cssls", "emmet_ls", "vue_ls", "gopls" },
 			})
 		end,
 	},
