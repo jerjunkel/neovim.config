9847ad6539e6a5a012c16ab3617558a31d924aea
Jermaine Kelly <jermaine.kelly@stellae.us>
2025-06-20 13:15:14 -0400

    feat: add sql file formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,22 +1,23 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				html = { "prettierd" },
-				css = { "prettierd" },
-				scss = { "prettierd" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				typescript = { "prettierd", "prettier", stop_after_first = true },
-				go = { "golines" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        html = { "prettierd" },
+        css = { "prettierd" },
+        scss = { "prettierd" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        go = { "golines" },
+        sql = { "sql_formater" }
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
+  end,
 }
