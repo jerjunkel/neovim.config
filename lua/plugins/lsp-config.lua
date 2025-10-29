085d2bf650ee483b06780e96ec4eb9775be60127
Jermaine Kelly <jermaine.kelly@stellae.us>
2025-10-29 09:26:33 -0400

    fix: use new lsp-config format

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,82 +1,95 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "cssls", "emmet_ls", "vue_ls", "gopls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls", "cssls", "emmet_ls", "vue_ls", "gopls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- local lspconfig = require("lspconfig")
 
-			vim.api.nvim_create_autocmd("LspAttach", {
-				desc = "LSP actions",
-				callback = function(event)
-					local opts = { buffer = event.buf }
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "LSP actions",
+        callback = function(event)
+          local opts = { buffer = event.buf }
 
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-					vim.keymap.set("n", "go", vim.lsp.buf.type_definition, {})
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-					vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, {})
-					vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
-					vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-				end,
-			})
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+          vim.keymap.set("n", "go", vim.lsp.buf.type_definition, {})
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+          vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, {})
+          vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
+          vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+      })
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.volar.setup({
-				init_options = {
-					typescript = {
-						tsdk = os.getenv("HOME")
-							.. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib",
-					},
-				},
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.emmet_ls.setup({
-				capabilities = capabilities,
-				filetypes = {
-					"css",
-					"html",
-					"javascript",
-					"less",
-					"sass",
-					"scss",
-					"vue",
-				},
-				init_options = {
-					html = {
-						options = {
-							["bem.enabled"] = true,
-						},
-					},
-				},
-			})
-		end,
-	},
+      vim.lsp.enable({
+        "lua_ls", "ts_ls", "cssls", "emmet_ls", "vue_ls", "gopls"
+      })
+
+      vim.lsp.config('volar', {
+        init_options = {
+          typescript = {
+            tsdk = os.getenv("HOME")
+                .. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib",
+          },
+        },
+        -- 	capabilities = capabilities,
+      })
+      -- lspconfig.lua_ls.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      -- lspconfig.ts_ls.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      -- lspconfig.volar.setup({
+      -- 	init_options = {
+      -- 		typescript = {
+      -- 			tsdk = os.getenv("HOME")
+      -- 				.. "/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib",
+      -- 		},
+      -- 	},
+      -- 	capabilities = capabilities,
+      -- })
+      -- lspconfig.gopls.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      -- lspconfig.cssls.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      vim.lsp.config('emmet_ls', {
+        capabilities = capabilities,
+        filetypes = {
+          "css",
+          "html",
+          "javascript",
+          "less",
+          "sass",
+          "scss",
+          "vue",
+        },
+        init_options = {
+          html = {
+            options = {
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
+    end,
+  },
 }
